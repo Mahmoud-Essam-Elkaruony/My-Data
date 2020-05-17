fc475d183de360878cf0b8ac5aa5e77bafa0fb0b
Mahmoud Essam EL-karuony <65455064+Mahmoud-Essam-Elkaruony@users.noreply.github.com>
2020-05-17 19:49:26 +0200

    Add files via upload

---
+++ Bootstrap/Create Scroll To Top Button/ Create Scroll To Top Button.js
@@ -0,0 +1,76 @@
+
+// How to controle about carousel time.
+
+$(document).ready(function(){
+
+    $(".carousel").carousel({
+
+        interval: 5000
+    });
+
+    // Show color option div when click on the gear
+
+    $(".gear-check").click(function()
+    {
+        $(".color-option").fadeToggle();
+    });
+
+    // Change theme color on click
+
+    let colorLi = $('.color-option ul li')
+    colorLi
+    .eq(0).css("backgroundColor","#E41B17").end()
+    .eq(1).css("backgroundColor","purple").end()
+    .eq(2).css("backgroundColor","blue").end()
+    .eq(3).css("backgroundColor","yellowgreen");
+
+    colorLi.click(function()
+    {
+        $("link[href*='theme']").attr("href", $(this).attr("data-valu"));
+
+    });
+
+    // Caching the scroll top element
+
+    let scrollButton = $("#scroll-top");
+
+    $(window).scroll(function()
+    {
+
+        if ( $(this).scrollTop() >= 1000 )
+        {
+            scrollButton.show();
+        }
+        else
+        {
+            scrollButton.hide();
+        }
+
+    });
+
+    // Click on the button to scroll top 
+
+    scrollButton.click(function()
+    {
+        $("html,body").animate({ scrollTop: 0}, 600);
+    });
+  
+
+});
+
+// Loading Screen
+
+$(window).load(function()
+{
+    // Show the scroll
+    $('body').css("overflow","auto");
+
+
+    $('.loading-overlay .spinner').fadeOut(2000, function() // Will fadeOut h1
+    {
+        $(this).parent().fadeOut(2000, function() // Will fadeOut Section 
+        {
+            $(this).remove(); // Will remove the Section 
+        });
+    });
+});
